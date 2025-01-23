d0e18de8a70d065bbc1756d8d76b2bdb7f312419
unique05 <perfectok05@gmail.com>
2025-01-23 11:55:06 +0100

    use lombok

--- src/main/java/com/unique05/ParkingLotManagementSystemRestApi/Entity/ParkingSpots.java
+++ src/main/java/com/unique05/ParkingLotManagementSystemRestApi/Entity/ParkingSpots.java
@@ -1,11 +1,13 @@
 package com.unique05.ParkingLotManagementSystemRestApi.Entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table
+@Data
 public class ParkingSpots {
 
     @Id
@@ -19,51 +21,6 @@ public class ParkingSpots {
     private int parkingSpotId;
     @Enumerated(EnumType.STRING)
     private TypeOfCar typeOfCar;
-    private boolean occupied;
+    private Boolean occupied;
     private String carPlateNumber;
-
-
-    public ParkingSpots(){}
-
-    public ParkingSpots(int parkingSpotId, TypeOfCar typeOfCar, boolean occupied, String carPlateNumber) {
-        this.parkingSpotId = parkingSpotId;
-        this.typeOfCar = typeOfCar;
-        this.occupied = occupied;
-        this.carPlateNumber = carPlateNumber;
-    }
-
-    public int getParkingSpotId() {
-        return parkingSpotId;
-    }
-
-    public void setParkingSpotId(int parkingSpotId) {
-        this.parkingSpotId = parkingSpotId;
-    }
-
-    public TypeOfCar getTypeOfCar() {
-        return typeOfCar;
-    }
-
-    public void setTypeOfCar(TypeOfCar typeOfCar) {
-        this.typeOfCar = typeOfCar;
-    }
-
-    public boolean getOccupied() {
-        return occupied;
-    }
-
-    public void setOccupied(boolean occupied) {
-        this.occupied = occupied;
-    }
-
-    public String getCarPlateNumber() {
-        return carPlateNumber;
-    }
-
-    public void setCarPlateNumber(String carPlateNumber) {
-        this.carPlateNumber = carPlateNumber;
-    }
-//    public ParkingSpots orElse(Object o) {
-//        return null;
-//    }
 }
